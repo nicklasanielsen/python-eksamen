b4155c7b8e9216b93de225d32e019bd0343e6769
Haugaard <mathiashn9@gmail.com>
2021-05-25 17:54:42 +0200

    Progress 2.0

--- Data_Grabber.py
+++ Data_Grabber.py
@@ -1,3 +1,4 @@
+from types import DynamicClassAttribute
 import bs4
 import requests
 import re
@@ -25,11 +26,80 @@ def select_Incident_Text ():
 def select_Incident_Header ():
     soup = read_page()
     header = soup.findAll('h3', attrs={'class' : None})
-    return header
+
+    soup_str = soup_to_string(header)
+        
+    soup_str = soup_str.replace("<h3>", "").replace("</h3>", "").replace("\n", "").replace("/","").replace("\xa0","")
+
+    return soup_str
 
 def select_All_Text ():
     soup = read_page()
     all_Text = soup.get_text()
     all_Text = all_Text.replace("\t", "").replace("\r", "").replace("\n", "")
     return all_Text
+
+def select_Incident ():
+    soup = read_page()
+    incident = soup.select('div[class="rich-text"]')
+
+    return incident
+
+def incident_Locations():
+    regions = select_Incident_Header()
+    cities = select_Incident()
+
+    cities = soup_to_string(cities)
+
+    list_of_cities = re.findall(r'\d{4} [A-Z][^\s]+', cities)
+    list_of_regions = re.findall('[A-Z][^A-Z]*', regions)
+
+    list_of_dicts = []
+
+    region = list_of_regions[0]
+    city_and_zipcode = list_of_cities[0]
+    crimes = 0
+    
+
+    for i in range (len(list_of_cities)):
+        dictt = {"Region" : region, "By og postnummer" : city_and_zipcode, "Antal af forbrydelser": crimes}
+
+
+
+        if dictt.get("By og postnummer") == list_of_cities[i]:
+            crimes +=1
+            dictt.update({"Antal af forbrydelser": crimes})
+        else:
+            dictt.update({"By og postnummer": list_of_cities[i]})
+            city_and_zipcode = dictt.get("By og postnummer")
+            crimes = 1
+            dictt.update({"Antal af forbrydelser": crimes})
+
+        list_of_dicts.append(dictt)
+
+    return list_of_dicts
+
+def incident_Time():
+    times = select_Incident()
+    
+    soup_str = soup_to_string(times)
+
+    soup_str = soup_str.replace("<h3>", "").replace("</h3>", "").replace("\n", "").replace("/","").replace("\xa0","").replace("<p>", "")
+
+    list_anmeldt = re.findall(r'\bAnmeldt: \d{8} \d{2}:\d{2}', soup_str)
+    list_sket = re.findall(r'\bSket: \d{8} \d{2}:\d{2}', soup_str)
+    list_of_sets = []
+
+    for i in range (len(list_anmeldt)):
+        incident_set = {list_anmeldt[i], list_sket[i]}
+        list_of_sets.append(incident_set)
+
+    return list_of_sets
+
+def soup_to_string(soup_list):
+    soup_str = ""
+    for i in soup_list:
+        soup_str = soup_str + str(i)
+
+    return soup_str
 
